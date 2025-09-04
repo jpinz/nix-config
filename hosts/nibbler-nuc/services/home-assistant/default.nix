74b2d3fa22d0791124e7381cb5252a773076b112
Julian Pinzer <julian@jpinzer.me>
2025-09-04 13:21:54 -0400

    Add minimal home-assistant config

--- hosts/nibbler-nuc/services/home-assistant/default.nix
+++ hosts/nibbler-nuc/services/home-assistant/default.nix
@@ -1,22 +1,97 @@
-{ pkgs, config, ... }:
+{ config, ... }:
 {
+  imports = [
+    # ./abode.nix
+    # ./blinds.nix
+    ./cloud.nix
+    ./default-config.nix
+    # ./esphome.nix
+    # ./hvac.nix
+    # ./leak.nix
+    # ./lights.nix
+    # ./miele.nix
+    # ./mqtt.nix
+    # ./notify.nix
+    # ./people.nix
+    # ./recorder.nix
+    # ./sonos.nix
+    # ./unifi.nix
+    # ./vacation.nix
+    # ./waste-collection.nix
+    # ./weather.nix
+  ];
+
   services.home-assistant = {
     enable = true;
     extraComponents = [
-      # Components required to complete the onboarding
-      "analytics"
-      "google_translate"
-      "met"
-      "radio_browser"
-      "shopping_list"
-      # Recommended for fast zlib compression
-      # https://www.home-assistant.io/integrations/isal
-      "isal"
+      # "adguard"
+      # "aladdin_connect"
+      # "androidtv_remote"
+      # "august"
+      # "cast"
+      # "econet"
+      # "google_translate"
+      # "homeassistant_hardware"
+      # "homeassistant_sky_connect"
+      # "homekit_controller"
+      # "isal"
+      # "kegtron" # To avoid "Component not found: kegtron"
+      # "met"
+      # "mqtt"
+      # "notify"
+      # "opower"
+      # "otp"
+      # "plex"
+      # "radarr"
+      # "radio_browser"
+      # "roborock"
+      # "roomba"
+      # "sabnzbd"
+      # "sonarr"
+      # "spotify"
+      # "yalexs_ble"
+      # "zeroconf"
     ];
+    # customComponents = with pkgs.home-assistant-custom-components; [
+    #   adaptive_lighting
+    #   better_thermostat
+    #   climate_group
+    #   emporia_vue
+    #   fellow
+    #   garmin_connect
+    #   spook
+    #   waste_collection_schedule
+    # ];
     config = {
-      # Includes dependencies for a basic setup
-      # https://www.home-assistant.io/integrations/default_config/
-      default_config = {};
+      http = {
+        trusted_proxies = [
+          "127.0.0.1"
+          "::1"
+        ];
+        use_x_forwarded_for = true;
+      };
+      homeassistant = {
+        auth_mfa_modules = [
+          { type = "totp"; }
+          { type = "notify"; }
+        ];
+      };
+      "automation manual" = [ ];
+      "automation ui" = "!include automations.yaml";
+      "scene manual" = [ ];
+      "scene ui" = "!include scenes.yaml";
     };
   };
+
+
+  # networking.firewall = {
+  #   allowedTCPPorts = [ 20163 ];
+  #   allowedUDPPorts = [ 5353 ];
+  # };
+
+  systemd.tmpfiles.rules = [
+    "f ${config.services.home-assistant.configDir}/automations.yaml 0755 hass hass"
+    "f ${config.services.home-assistant.configDir}/scenes.yaml 0755 hass hass"
+    "f ${config.services.home-assistant.configDir}/secrets.yaml 0755 hass hass"
+  ];
 }
